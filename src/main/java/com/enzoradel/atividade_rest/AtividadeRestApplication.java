32ef40f3da7cdb99f16ad11f27f7691879d3d42b
Enzo Radel <enzo.radel@webpag.com.br>
2025-08-30 09:55:11 -0300

    initial commit

---
+++ src/main/java/com/enzoradel/atividade_rest/AtividadeRestApplication.java
@@ -0,0 +1,13 @@
+package com.enzoradel.atividade_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzoradel/atividade_rest/AtividadeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzoradel.atividade_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
